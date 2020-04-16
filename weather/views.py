ffaf1dbb83143fbe9e8758d48ab0ead66074b0b0
cecileh <cecileh@exexample.com>
2020-04-16 19:10:38 +0200

    Display weather of several cities

--- weather/views.py
+++ weather/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e8a038486e11484605665001d81b4a85'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json() # request the API data and convert the JSON to Python data types
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
+    cities = City.objects.all() #return all the cities in the database
+    weather_data = []
+    
+    for city in cities:
+        
+        city_weather = requests.get(url.format(city)).json() # request the API data and convert the JSON to Python data types
+        
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        
+        weather_data.append(weather) # add the data for the current city into our list
+        
     context = {
-        'weather': weather
+        'weather_data': weather_data
     }
     
     return render(request, 'weather/index.html', context) # return the index.html template
