b8830bf164ed7b1a5cd8ac3218cb1d8a7f980c03
cecileh <cecileh@exexample.com>
2020-04-17 11:23:11 +0200

    Add form and translate in French

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e8a038486e11484605665001d81b4a85'
     cities = City.objects.all() #return all the cities in the database
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=e8a038486e11484605665001d81b4a85&lang=fr'
+   
+    if request.method == 'POST': # only true if the form is submitted
+        form = CityForm(request.POST) # add actual request data to form processing
+        form.save() # will validate and save if validate
+        
+    form = CityForm()
+    
     weather_data = []
     
     for city in cities:
@@ -13,16 +22,18 @@ def index(request):
         city_weather = requests.get(url.format(city)).json() # request the API data and convert the JSON to Python data types
         
         weather = {
-            'city': city,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
         }
         
         weather_data.append(weather) # add the data for the current city into our list
         
     context = {
-        'weather_data': weather_data
+        'weather_data': weather_data,
+        'form': form
     }
+    
     
     return render(request, 'weather/index.html', context) # return the index.html template
